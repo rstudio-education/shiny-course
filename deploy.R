1e46b38acd0f631df24aad125e6fa31ce2e0b0de
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-04-23 15:03:10 +0100

    Use deployDoc instead

--- deploy.R
+++ deploy.R
@@ -2,36 +2,31 @@ library(rsconnect)
 
 # Module 1 ---------------------------------------------------------------------
 
-deployApp(
-  appDir = "01-hello",
-  appPrimaryDoc = "01-hello.Rmd",
+deployDoc(
+  doc = "01-hello/01-hello.Rmd",
   account = "minecr",
   forceUpdate = TRUE
 )
 
 # Module 2 ---------------------------------------------------------------------
 
-deployApp(
-  appDir = "02-flow",
-  appPrimaryDoc = "02-flow.Rmd",
-  account = "minecr",
+deployDoc(
+  doc = "02-flow.Rmd/02-flow.Rmd",
   forceUpdate = TRUE
 )
 
 # Module 3 ---------------------------------------------------------------------
 
-deployApp(
-  appDir = "03-reactivity",
-  appPrimaryDoc = "03-reactivity.Rmd",
+deployDoc(
+  doc = "03-reactivity/03-reactivity.Rmd",
   account = "minecr",
   forceUpdate = TRUE
 )
 
 # Module 4 ---------------------------------------------------------------------
 
-deployApp(
-  appDir = "04-ui",
-  appPrimaryDoc = "04-ui.Rmd",
+deployDoc(
+  doc = "04-ui/04-ui.Rmd",
   account = "minecr",
   forceUpdate = TRUE
 )
